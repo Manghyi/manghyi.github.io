9b7902db3c56a97d4aa053b56011e5879347a20d
manghyi <chiwon618@gmail.com>
2026-05-04 09:27:06 +0900

    init github action setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+const repoName = "manghyi";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",          // 정적 export
+  basePath: `/${repoName}`, 
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,     // ✅ next/image 사용 시 필수
+  },
 };
 
 export default nextConfig;
