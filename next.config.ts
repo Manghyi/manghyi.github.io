97843541685ed354122841214dc22eec3c0724ef
manghyi <chiwon618@gmail.com>
2026-05-12 21:57:48 +0900

    fix: basePath 삭제

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",          // 정적 export
-  basePath: isProd ? `/${repoName}` : "",
   images: {
     unoptimized: true,     // ✅ next/image 사용 시 필수
   },
