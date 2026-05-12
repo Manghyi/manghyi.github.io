7fc70dcb86c9985f8959dfd243ed1d7f0acbeb0f
Manghyi <Manghyi@users.noreply.github.com>
2026-05-12 12:21:41 +0000

    deploy: 149748bba589664b68c9c1b77672ea642de63fd6

--- _next/static/usiZamqm-H0CODjjLphU3/_buildManifest.js
+++ _next/static/usiZamqm-H0CODjjLphU3/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/manghyi//_next/:path+",
-        "destination": "/manghyi/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
