895f1a1e2233faa2e550beb18f5b5f48c6a89cfb
manghyi <chiwon618@gmail.com>
2026-05-13 23:16:38 +0900

    dev: 프로젝트 페이지 구성 요소 러프

---
+++ app/project/page.tsx
@@ -0,0 +1,36 @@
+const project = () => {
+    return (
+        <main>
+            <div>
+                <img src="" />
+            </div>
+            <h2>프로젝트</h2>
+            <div>
+                <h3>프로젝트 기간</h3>
+                <p>2026.01.01 - 2026.04.01</p>
+            </div>
+            <div>
+                <h3>설명</h3>
+                <p>이 프로젝트는...</p>
+            </div>
+            <div>
+                <h3>스펙</h3>
+                <ul>
+                    <li><p>React</p></li>
+                    <li><p>SASS</p></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Link</h3>
+                <ul>
+                    <li>
+                        <p>Github</p>
+                        <p>https://...</p>
+                    </li>
+                </ul>
+            </div>
+        </main>
+    );
+}
+
+export default project;
