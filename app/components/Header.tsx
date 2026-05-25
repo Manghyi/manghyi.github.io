a39a35670706d679523c5fca34f2f04e03d4ee14
manghyi <chiwon618@gmail.com>
2026-05-26 08:17:59 +0900

    dev: Main header nav UI change from link to button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,30 @@
+'use client'
+
 // components/Header.ts
 import styles from '../styles/Home.module.scss';
 
 const Header = () => {
+  function moveScroll(id: string) {
+    const target = document.getElementById(id);
+
+    if (!target) return;
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+
+    history.replaceState(null, "", `#${id}`);
+  };
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} sticky top-0 z-50 h-20`}>
       <h1><img src="/images/my_logo.png" /></h1>
       <nav>
         <ul className={styles.navList}>
-          <li><a href="#aboutMe" >About Me</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
+          <li><button onClick={() => moveScroll("aboutMe")}>About Me</button></li>
+          <li><button onClick={() => moveScroll("skills")}>Skills</button></li>
+          <li><button onClick={() => moveScroll("projects")}>Projects</button></li>
           {/* <li><a href="#Career">Career</a></li> */}
         </ul>
       </nav>
