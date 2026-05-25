06ec2a2043f879016bde0f4a73024e138306f32e
manghyi <chiwon618@gmail.com>
2026-05-26 08:39:17 +0900

    dev: section project UI change

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,26 +1,46 @@
+'use client'
+
 // components/ProjectCard.js
 import styles from '../styles/Home.module.scss';
+import { useRouter } from "next/navigation";
 
 interface ProjcetType {
   title: string,
   image: string,
   description: string,
   detail?: string,
+  start: string,
+  end?: string
 }
 
 const ProjectCard = ({ project }: { project: ProjcetType }) => {
+  const router = useRouter();
+
+  function goMore() {
+    if (!project.detail) return;
+
+    router.push(project.detail);
+  }
+
   return (
     <div className={styles.projectCard}>
-      <div className={styles.projectCardImage}>
+      <div className={`${styles.projectCardImage} border-1`}>
         <img src={project.image} alt={project.title} />
       </div>
       <h4 title={project.title}>{project.title}</h4>
       <div className={styles.projectCardDescription}>
         <p>{project.description}</p>
       </div>
-      <div>
-        <a href={project.detail}>more &gt;</a>
+      <div className="flex">
+        <p>기간 :</p>
+        <div className="flex ml-2">
+          <p>{project.start}</p>
+          {project.end ? <p className="ml-1">{` - ${project.end}`}</p> : <></>}
+        </div>
       </div>
+      {/* <div>
+        <button onClick={() => goMore()}>more &gt;</button>
+      </div> */}
     </div>
   );
 };
